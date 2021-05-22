d1c449c7bf953d45e4190ea1f2921990dead2b39
HairumiRasyad <heruputra27@gmail.com>
2021-05-22 14:56:12 +0700

    default

---
+++ resources/views/Artikel0114.blade.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Artikel</title>
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="/">Home</a>
+        <a class="nav-link" href="/Artikel">Artikel</a>
+        <a class="nav-link" href="/Contact">Contact</a>
+      </div>
+    </div>
+  </div>
+</nav>
+  
+  <div class="container">
+    <div class="row">
+        <div class="col-10">
+             <<h1 class="mt-3">APA ITU FRAMEWORK ?</h1>
+              <p>Secara umum, framework adalah struktur nyata atau konseptual yang dimaksudkan berfungsi sebagai pendukung atau panduan
+              membangun dan memperluas struktur menjadi sesuatu yang bermanfaat. Dalam sistem komputer, istilah framework memiliki makna yang sedikit berbeda.</p>
+         <h2 class="mt-2">1. Pengertian Framework</h2> 
+              <p>framework adalah fondasi dengan tingkat kerumitan yang ditentukan dan dapat diperluas oleh pemrogram dengan menggunakan kode mereka sendiri termasuk kompiler, juru bahasa, atau API.
+              Secara umum, framework menyediakan lingkungan yang memfasilitasi jenis pemrograman tertentu untuk proyek pengembangan software.</p>
+              <p>Kerangka kerja diciptakan untuk mempermudah kinerja dari programmer. Sehingga, seorang programmer tidak perlu untuk menuliskan kode 
+              secara berulang – ulang. Karena di dalamnya sendiri anda hanya perlu menyusun komponen – komponen pemrograman saja.</p>
+         <h3 class="mt-2">2. Fungsi Framework</h3>
+              <p>Sebagai developer, tentu harus mengetahui tujuan dari penggunaannya untuk kepentingan pembuatan aplikasi. Sehingga, untuk proses pengerjaan 
+              aplikasi dapat dilakukan dengan menggunakan framework yang tepat dan sesuai dengan kebutuhan project. Berikut merupakan beberapa fungsi kerangka 
+              kerja dalam web development.</p>
+              PHP yang dikenal sebagai bahasa server-side scripting paling populer di dunia telah banyak berkembang sejak potongan kode inline pertama muncul dalam file HTML statis.
+        <h4 class="mt-2">Framework PHP terbaik</h4>
+              <p>PHP yang dikenal sebagai bahasa server-side scripting paling populer di dunia telah banyak berkembang sejak potongan kode inline pertama muncul dalam file HTML statis.</p>
+              <p>Pengembang perlu membangun situs web dan aplikasi yang kompleks dan dengan kerumitan tertentu mungkin butuh terlalu banyak waktu untuk selalu memulai dari awal,
+              sehingga muncul kebutuhan akan cara pengembangan yang lebih terstruktur.</p>
+              <p>Framework PHP memberi solusi yang memudahkan untuk membangun situs web dan aplikasi yang kompleks.</p>
+          <h4 class="mt-2">- Kode Program Lebih Terstruktur</h4>
+              <p>Fungsi framework yang utama adalah membuat source code menjadi lebih terstruktur. Terstruktur disini, berarti program yang dibuat akan 
+              dimasukkan ke dalam setiap komponen sesuai dengan fungsi nya masing – masing.</p>
+              <p>Salah satu contoh dari kode program terstruktur dapat dilihat dari framework PHP, yaitu Laravel. Yang menggunakan konsep paradigma MVC 
+              (Model, View, Controller). Terdapat tiga komponen utama untuk mengembangkan website menggunakan model framework tersebut.Model berfungsi untuk 
+              tempat atau wadah menampung kode program berupa algoritma pemrograman dan penghubung database aplikasi. View berfungsi sebagai wadah menampung kode 
+              program untuk membuat tampilan yang nantinya ditampilkan kepada customer / client. Dan controller berfungsi untuk menghubungkan model dan view agar menjadi sebuah website secara keseluruhan.</p>
+          <h5 class="mt-2">- Membantu Kinerja Developer</h5>
+              <p>Fungsi yang kedua adalah membantu kinerja dari developer sendiri. Dari sini, anda pasti sudah berpikir bahwa sebenarnya dalam membuat sebuah aplikasi 
+              dapat dilakukan tanpa menggunakan bantuan framework. Tentu saja, bisa dilakukan jika proyek yang ditangani dalam lingkup kecil.
+              Apabila anda membuat aplikasi atau tampilan website untuk sebuah perusahaan atau organisasi besar tentu saja hal tersebut tidak disarankan bahkan mindset 
+              tersebut harus segera diubah. Framework diciptakan untuk memudahkan kinerja dari developer dalam segi efisiensi waktu serta resource yang dibutuhkan.</p>
+          <h6 class="mt-2">- Meningkatkan keamanan perangkat lunak / website</h6>
+              <p>Selain membantu kinerja dari developer, dari segi keamanan website maupun software juga akan meningkat. Karena dalam framework telah diidentifikasi oleh beberapa developer ahli dari berbagai negara.
+              Kemudian, sebuah kerangka kerja juga memiliki beberapa versi yang setiap saat akan selalu ada pembaruan dari segi fitur serta untuk mengurangi adanya bugs. 
+              Keamanan akan selalu diperbarui dan dimonitoring agar tidak terjadi permasalahan yang lebih serius pada website misalnya terjadinya hacking maupun peretasan data.</p>
+          <h7 class="mt-2">- Pemeliharaan dan dokumentasi dari website lebih mudah</h7>
+          <p>Dengan melakukan maintenance, anda dapat mengubah versi website tersebut dan menambahkan beberapa fitur dengan lebih mudah dan aman. Sehingga, ketika ada perbaikan pada website maka terdapat notifikasi 
+          atau pesan bagi pengguna bahwa website masih dalam perbaikan.Selanjutnya, dari segi dokumentasi juga lebih terstruktur. Anda akan sangat kerepotan apabila dalam proses dokumentasi tidak menggunakan bantuan framework. 
+          Dalam kerangka kerja, setiap dokumen aplikasi yang dibangun dapat diidentifikasi dengan mudah dan cepat.</p>
+        </div>
+    </div>
+  </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
